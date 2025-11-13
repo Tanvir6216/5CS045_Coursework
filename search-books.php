38adeb90120494d1ded92b92b6622148b988feee
Shahrier Ahammed Tanvir <s.a.tanvir@wlv.ac.uk>
2025-11-13 15:18:55 +0000

    Update search-books.php
    I make some changes in search option so that i can specifically search movies by both Genre and Year

--- search-books.php
+++ search-books.php
@@ -2,23 +2,43 @@
   // Connect to database and run SQL query
   include("db.php");
 
-  // Read value from form
+  // Read values from form (we’ll add genre and year support)
   $keywords = isset($_POST['keywords']) ? $_POST['keywords'] : '';
+  $genre = isset($_POST['genre']) ? $_POST['genre'] : '';
+  $year = isset($_POST['year']) ? $_POST['year'] : '';
 
-  // Run SQL query 
-  $sql = "SELECT * FROM books 
-          WHERE Title LIKE '%{$keywords}%' 
-          OR Author LIKE '%{$keywords}%'
-          OR Genre LIKE '%{$keywords}%'
-          OR Description LIKE '%{$keywords}%'
-          OR Actions LIKE '%{$keywords}%'
-          ORDER BY Published_date DESC";
-          
+  // Build SQL query dynamically
+  $sql = "SELECT * FROM books WHERE 1=1";
+
+  if (!empty($keywords)) {
+    $sql .= " AND (Title LIKE '%{$keywords}%' 
+              OR Author LIKE '%{$keywords}%'
+              OR Description LIKE '%{$keywords}%')";
+  }
+
+  if (!empty($genre)) {
+    $sql .= " AND Genre LIKE '%{$genre}%'";
+  }
+
+  if (!empty($year)) {
+    $sql .= " AND YEAR(Published_date) = '{$year}'";
+  }
+
+  $sql .= " ORDER BY Published_date DESC";
+
   $results = mysqli_query($conn, $sql);
 ?>
 
 <!-- Added link for Add New Book -->
 <a href="add-books.php" class="btn btn-success mb-3">+ Add New Book</a>
+
+<!-- ✅ Add extra search inputs -->
+<form method="post" class="mb-3">
+  <input type="text" name="keywords" placeholder="Search title/author..." value="<?= htmlspecialchars($keywords) ?>">
+  <input type="text" name="genre" placeholder="Genre" value="<?= htmlspecialchars($genre) ?>">
+  <input type="number" name="year" placeholder="Year (e.g. 2023)" value="<?= htmlspecialchars($year) ?>">
+  <button type="submit" class="btn btn-primary btn-sm">Search</button>
+</form>
 
 <table class="book-table"> 
   <tr>
@@ -31,4 +51,3 @@
     </tr>
   <?php endwhile; ?>
 </table>
-
